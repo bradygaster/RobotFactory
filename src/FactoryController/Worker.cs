c485e306078d8c9caefc93344c8827673b95b958
Brady Gaster <bradyg@microsoft.com>
2019-04-11 01:27:22 -0700

    changed to use storage directly

--- src/FactoryController/Worker.cs
+++ src/FactoryController/Worker.cs
@@ -6,11 +6,16 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
 
 namespace FactoryController
 {
     public class Worker : BackgroundService
     {
+        private const string DASHBOARD_QUEUE_CONNECTION_STRING = "DASHBOARD_QUEUE_CONNECTION_STRING";
+        private const string DASHBOARD_QUEUE = "DASHBOARD_QUEUE";
+
         private readonly ILogger<Worker> _logger;
         HttpClient _httpClient;
 
@@ -33,11 +38,33 @@ namespace FactoryController
                         
                     var result = await _httpClient.GetAsync("http://dashboard");
                     _logger.LogInformation($"HTTP response from dashboard: {result.StatusCode}");
+
+                    await EnqueueDashboardPing();
                 }
                 catch(Exception ex)
                 {
                     _logger.LogError(ex, "Error during dashboard test");
                 }
+            }
+        }
+
+        private async Task EnqueueDashboardPing()
+        {
+            _logger.LogInformation("Sending dashboard heartbeat at: {time}", DateTimeOffset.Now);
+
+            CloudStorageAccount storageAccount = null;
+
+            var success = CloudStorageAccount.TryParse(
+                Environment.GetEnvironmentVariable("DASHBOARD_QUEUE_CONNECTION_STRING"),
+                out storageAccount
+            );
+
+            if(success)
+            {
+                var queueClient = storageAccount.CreateCloudQueueClient();
+                var queue = queueClient.GetQueueReference(DASHBOARD_QUEUE);
+                await queue.CreateIfNotExistsAsync();
+                await queue.AddMessageAsync(new CloudQueueMessage("FactoryControllerHeartbeat"));
             }
         }
     }
